9db712a567030ca74ddc971d9473f8835e88d141
Steven Cheng <cheng2018@gmail.com>
2024-02-15 21:31:18 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,12 @@
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts';
+/* import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Script from 'next/script';
+ */
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <head>
+        <Script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous"></Script>
+      </head> */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
